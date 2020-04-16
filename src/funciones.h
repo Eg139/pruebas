c637e27dbdfc05731675c436736c3470b8f72f75
Eric Guzman <53089003+Eg139@users.noreply.github.com>
2020-04-16 17:39:25 -0300

    menu en funciones

--- src/funciones.h
+++ src/funciones.h
@@ -7,3 +7,4 @@ float resta(float num,float num1);
 float multiplicacion(float num,float num1);
 int validacionFactorial(float numero);
 float validacionDivision (float num, float num1);
+void menu(float num, float num1);
